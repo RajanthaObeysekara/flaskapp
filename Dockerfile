cf86bf05a78ec651255fd94c43592f6ba6f71d34
Rajantha <2015s15221@stu.cmb.ac.lk>
2021-08-26 09:32:16 +0000

    first commit

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM ubuntu
+RUN apt-get update -y && \
+    apt-get install -y python3-pip python-dev
+
+COPY ./requirements.txt /app/requirements.txt
+WORKDIR /app
+RUN pip install -r requirements.txt
+COPY . /app
+ENTRYPOINT ["python"]
+CMD ["app.py"]
---
+++ app.py
@@ -0,0 +1,13 @@
+# Imports necessary libraries
+from flask import Flask 
+# Define the app
+app = Flask(__name__)
+
+# Get a welcoming message once you start the server.
+@app.route('/')
+def home():
+    return 'Home sweet home!'
+
+# If the file is run directly,start the app.
+if __name__ == '__main__':
+    app.run(debug=True,host="0.0.0.0")
